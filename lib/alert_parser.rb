47723e548afc7c0952f604483c5ec28437390c1d
davidkelley <david.james.kelley@gmail.com>
2016-09-28 07:49:56 +0100

    v0.0.3

--- lib/alert_parser.rb
+++ lib/alert_parser.rb
@@ -7,6 +7,7 @@ module AlertParser
   autoload :Locale, 'alert_parser/locale'
 
   def self.parse(type, locale = 'en', currency_code = 'usd', data = {})
-    Parser.get(type).new(Locale.new(language: locale), currency_code, data)
+    props = { locale: Locale.new(language: locale), currency_code: currency_code, data: data }
+    Parser.get(type).new(props)
   end
 end
