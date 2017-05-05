37ab123f650a8ab8ab60e3054f80cc9f05e9a286
Mark Stratmann <mark@stratmann.me.uk>
2017-05-05 12:46:58 +0100

    Revert "Get translations externally"
    This reverts commit 8095cf53e19555f5ade171ab4fba9ba12b8c968a.

--- lib/alert_parser.rb
+++ lib/alert_parser.rb
@@ -6,8 +6,8 @@ module AlertParser
   autoload :Parser, 'alert_parser/parser'
   autoload :Locale, 'alert_parser/locale'
 
-  def self.parse(type, currency_code = 'usd', data = {}, translations = {})
-    props = { locale: Locale.new(translations: translations), currency_code: currency_code, data: data }
+  def self.parse(type, locale = 'en', currency_code = 'usd', data = {})
+    props = { locale: Locale.new(language: locale), currency_code: currency_code, data: data }
     Parser.get(type).new(props)
   end
 end
--- lib/alert_parser/locale.rb
+++ lib/alert_parser/locale.rb
@@ -1,13 +1,28 @@
+require_dependency 'shotgun'
 
 module AlertParser
   class Locale
 
-    def initialize(translations: {})
-      @translations = translations
+    @@translations = {}
+    @@translations_loaded = {}
+
+    LOCALES_PATH = ENV['LOCALES_PATH']
+
+    attr_reader :language
+
+    def initialize(language: 'en')
+      @language = language
     end
 
     def translations
-      @translations
+      # Get the desired language and store it for 20 minutes
+      if !@@translations[language] || !@@translations_loaded[language] || @@translations_loaded[language] < (Time.now.utc.to_i - (60 * 20) )
+        @@translations[language] = Services::I18n.get(locale: language).call.body[language.to_sym]
+        @@translations_loaded[language] = Time.now.utc.to_i
+      end
+
+      @@translations[language]
+
     end
 
     def format_currency(code, value, dp = 2)
@@ -36,5 +51,8 @@ module AlertParser
       translations['common']['currencies']['symbols'][code.to_s.downcase].dup
     end
 
+    # def file(locale = 'en')
+    #   @file ||= File.open(File.expand_path("#{LOCALES_PATH}/#{language}.yml", __FILE__)).read
+    # end
   end
 end
