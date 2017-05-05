306eb42c25bb4c433ac18b55847e17eb89b597e1
Mark Stratmann <mark@stratmann.me.uk>
2017-05-05 11:40:13 +0100

    Switch to api-i18n

--- lib/alert_parser/locale.rb
+++ lib/alert_parser/locale.rb
@@ -1,5 +1,9 @@
 module AlertParser
   class Locale
+
+    @@translations = {}
+    @@translations_loaded = {}
+
     LOCALES_PATH = ENV['LOCALES_PATH']
 
     attr_reader :language
@@ -9,7 +13,14 @@ module AlertParser
     end
 
     def translations
-      @translations ||= YAML::load(file)[language]
+      # Get the desired language and store it for 20 minutes
+      if !@@translations[language] || !@@translations_loaded[language] || @@translations_loaded[language] < (Time.now.utc.to_i - (60 * 20) )
+        @@translations[language] = Services::I18n.get(locale: language).call.body[language.to_sym]
+        @@translations_loaded[language] = Time.now.utc.to_i
+      end
+
+      @@translations[language]
+
     end
 
     def format_currency(code, value, dp = 2)
@@ -38,8 +49,8 @@ module AlertParser
       translations['common']['currencies']['symbols'][code.to_s.downcase].dup
     end
 
-    def file(locale = 'en')
-      @file ||= File.open(File.expand_path("#{LOCALES_PATH}/#{language}.yml", __FILE__)).read
-    end
+    # def file(locale = 'en')
+    #   @file ||= File.open(File.expand_path("#{LOCALES_PATH}/#{language}.yml", __FILE__)).read
+    # end
   end
 end
--- lib/alert_parser/version.rb
+++ lib/alert_parser/version.rb
@@ -1,3 +1,3 @@
 module AlertParser
-  VERSION = "0.0.9"
+  VERSION = "0.0.10"
 end
